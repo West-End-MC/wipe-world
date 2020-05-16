cfaf8b139dac3b82e8e7c377707d1994f6ef4eb9
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2020-05-15 21:04:37 -0400

    Update arguments parser

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
-import re
 parser = argparse.ArgumentParser(description='Browse mca files using block/chunk coordenates.')
-parser.add_argument("--mode", "-m", choices=["in", "out"], default="in", 
+parser.add_argument("--selection", "-s", choices=["in", "out"], default="in", 
                     help="""
                          Defines selection mode (default: "%(default)s")
                          in =>  Will find all the mca files that are within the coordinate range.
@@ -9,24 +8,23 @@ parser.add_argument("--mode", "-m", choices=["in", "out"], default="in",
                          """)
 parser.add_argument("--path", "-p", type=str, 
                     help="""
+                         Required if "--mode" is "out".
                          If defined, it will show only the mca files that are inside the folder.
                          If not defined, it will show all possible mca files.
                          """)
+parser.add_argument("--mode", "-m", type=str, choices=["blocks", "chunks"], default="blocks")
 
-subparsers = parser.add_subparsers()
-parser_block = subparsers.add_parser('block')
-parser_chunk = subparsers.add_parser('chunk')
-
-parser_block.add_argument("begin-x", type=int)
-parser_block.add_argument("begin-y", type=int)
-parser_block.add_argument("begin-z", type=int)
-parser_block.add_argument("end-x", type=int)
-parser_block.add_argument("end-y", type=int)
-parser_block.add_argument("end-z", type=int)
-
-parser_chunk.add_argument("begin-x", type=int)
-parser_chunk.add_argument("begin-z", type=int)
-parser_chunk.add_argument("end-x", type=int)
-parser_chunk.add_argument("end-z", type=int)
+parser.add_argument("begin-x", type=int)
+parser.add_argument("begin-y", type=int)
+parser.add_argument("begin-z", type=int)
+parser.add_argument("end-x",   type=int)
+parser.add_argument("end-y",   type=int)
+parser.add_argument("end-z",   type=int)
 
 args = parser.parse_args()
+
+if args.selection == "out" and args.path is None:
+    parser.error("--path is required if --mode is \"out\".")
+
+beg = { x: args.begin_x, y:args.begin_y, args.begin_z }
+end = { x: args.end_x, y:args.end_y, args.end_z }
