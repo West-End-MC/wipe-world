815c806bdab73879718a0a556cb0fb1664c064a2
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2020-05-15 22:02:39 -0400

    Allow to get region axis

--- main.py
+++ main.py
@@ -30,6 +30,12 @@ if args.selection == "out" and args.path is None:
 coordenates = [ { "x": args.begin_x, "y": args.begin_y, "z": args.begin_z },
                 { "x": args.end_x,   "y": args.end_y,   "z": args.end_z } ]
 
+min_block   = { "x": 0, "y": 0, "z": 0 }
+max_block   = { "x": 0, "y": 0, "z": 0 }
+
+min_chunk   = { "x": 0, "y": 0, "z": 0 }
+max_chunk   = { "x": 0, "y": 0, "z": 0 }
+
 # Ensuring that the axes are ordered from smallest to largest.
 for axis in ("x", "y", "z"):
     if coordenates[0][axis] > coordenates[1][axis]:
@@ -37,3 +43,21 @@ for axis in ("x", "y", "z"):
         coordenates[0][axis] = coordenates[1][axis]
         coordenates[1][axis] = temp
 
+# Convert blocks to chunks.
+if args.mode == "blocks":
+    for index in range(len(coordenates)):
+        for axis in ("x", "y", "z"):
+            coordenates[index][axis] = coordenates[index][axis] >> 4
+
+for axis in ("x", "y", "z"):
+    min_block[axis] =  coordenates[0][axis] << 4
+    max_block[axis] = (coordenates[1][axis] + 1 << 4) - 1
+
+# Convert chunks to regions.
+for index in range(len(coordenates)):
+    for axis in ("x", "y", "z"):
+        coordenates[index][axis] = coordenates[index][axis] >> 5
+
+for axis in ("x", "y", "z"):
+    min_chunk[axis] =  coordenates[0][axis] << 5
+    max_chunk[axis] = (coordenates[1][axis] + 1 << 5) - 1
