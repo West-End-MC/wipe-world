4da7221a299f24a4dfaec03f993da02d4b4fb339
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2020-05-18 20:27:31 -0400

    Add iteration to Selection Class

--- classes.py
+++ classes.py
@@ -9,4 +9,26 @@ class Selection:
     def toList(self):
         return [list(range(getattr(self.start, axis), getattr(self.end, axis) + 1)) for axis in ("x", "y", "z")]
 
+    def __iter__(self):
+        self.__current_x = self.start.x
+        self.__current_y = self.start.y
+        self.__current_z = self.start.z
+        return self
+
+    def __next__(self):
+        coordinate = Coordinate(self.__current_x, self.__current_y, self.__current_z)
+        if self.__current_x > self.end.x:
+            raise StopIteration
+        elif self.__current_y == self.end.y:
+            self.__current_x += 1
+            self.__current_y = self.start.y
+        elif self.__current_z == self.end.z:
+            self.__current_y += 1
+            self.__current_z = self.start.z
+        else:
+            self.__current_z += 1
+        
+        return coordinate
+
+
 
